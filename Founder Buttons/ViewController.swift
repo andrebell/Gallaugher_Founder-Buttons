f39c61d5166723849b80c7634097b566b5927dce
Andre Alexander Bell <github@andre-bell.de>
2022-01-02 21:28:16 +0100

    First draft of FounderButton

--- Founder Buttons/ViewController.swift
+++ Founder Buttons/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var founder1Label: UILabel!
+    @IBOutlet weak var founder2Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clear()
+    }
+    
+    func clear() {
+        founder1Label.text = ""
+        founder2Label.text = ""
     }
 
-
+    @IBAction func googleButtonPressed(_ sender: UIButton) {
+        founder1Label.text = "Larry Page"
+        founder2Label.text = "Sergey Brin"
+    }
+    
+    @IBAction func runwayButtonPressed(_ sender: UIButton) {
+        founder1Label.text = "Jennifer Hyman"
+        founder2Label.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        clear()
+    }
+    
 }
 
